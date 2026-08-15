a0a687fd816c0584b5b537271661ab12c27fddba
Gaurish-Goyal94 <Gaurishgoyal.35321@stu.upes.ac.in>
2026-08-15 08:29:45 +0000

    Add files via upload
    Program for sum, difference , product , and quotient

---
+++ day1q2.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main () {
+
+	int a;
+	int b;
+
+	printf("Enter two number to get their sum, difference, product and quotient : ");
+	scanf("%d %d",&a, &b);
+
+	int sum = a+b;
+	int difference = a-b;
+	int product = a*b;
+	int quotient = a/b;
+
+	printf("The sum, difference, product, quotient are : %d %d %d %d", sum, difference, product, quotient);
+	return 0;
+}
