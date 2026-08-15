6ea89d34946b0875d6fc8de64c0401a83070af68
Gaurish-Goyal94 <Gaurishgoyal.35321@stu.upes.ac.in>
2026-08-15 09:00:08 +0000

    Add files via upload
    permimatre and area of rectangle

---
+++ day2q1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main () {
+
+	int length;
+	int breadth;
+	printf("Enter the length of rectangle : ");
+	scanf("%d", &length);
+	printf("Enter the breadth of rectangle : ");
+	scanf("%d", &breadth);
+
+	int perimetre = 2*(length+breadth);
+	int area = length*breadth;
+
+	printf("The perimetre is %d, and the area is %d", perimetre, area);
+	return 0;
+}
